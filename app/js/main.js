bf71a4b212e11a1f60d195bdb0d8db322598068e
Andrey Ost <ost.and.g@gmail.com>
2019-09-03 22:50:46 +0300

    initial commit

---
+++ app/js/main.js
@@ -0,0 +1,4 @@
+import {a} from './file.js';
+
+console.log(a);
+
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const {task,src, dest, watch,series,parallel} = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const browserify = require('browserify');
+const sourcemaps = require('gulp-sourcemaps');
+const fs = require('fs');
+
+task('sass', function(){
+    gulp.src('sources-file')
+        .pipe(sass())
+        .pipe(dest('app/styles'));
+});
+
+task('html', function() {
+    return src('app/index.html')
+        .pipe(dest('dist/'))
+});
+
+
+task('css', function () {
+    return src('app/styles/styles.scss')
+        .pipe(sass({'outputStyle': 'compressed'}))
+        .pipe(dest('dist/'))
+});
+
+task('js', function () {
+    return browserify("./app/js/main.js")
+        .transform("babelify", {
+            presets: ["@babel/preset-env"],
+            sourceMaps: true
+        })
+        .bundle()
+        .pipe(fs.createWriteStream("dist/main.js"))
+});
+
+task('sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    watch(['app/index.html']).on('change', series('html', browserSync.reload));
+    watch(['app/styles/**/*.scss']).on('change', series('css', browserSync.reload));
+    watch(['app/js/**/*.js']).on('change', series('js', browserSync.reload));
+});
+
+exports.js = 'js';
+exports.build = series(parallel("html", "css", "js"), 'sync');
+exports.default = parallel("html", "css", "js");
